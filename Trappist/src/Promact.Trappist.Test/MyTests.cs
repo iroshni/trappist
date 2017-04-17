2aa6afd8f81a54b48b36e2c07bcd4de25455c5eb
Roshni Shah <roshni@promactinfo.com>
2017-04-17 14:13:35 +0530

    boiler plat new test proj

---
+++ Trappist/src/Promact.Trappist.Test/MyTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Promact.Trappist.Test
+{ 
+    public class MyTests
+    {
+        public MyTests()
+        {
+              
+        }
+
+        [Fact]
+        public void Add()
+        {
+            Assert.Equal(2, (4 - 2));
+        }
+    }
+}
