7826a1fcbf1376d3c611dbe0703c979ad77b17e9
Roshni Shah <roshni@promactinfo.com>
2017-04-17 15:18:56 +0530

    tried with new project

---
+++ Trappist/Promact.Trappist.UnitTests/MyTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Promact.Trappist.UnitTests
+{
+    public class MyTests
+    {
+        public MyTests()
+        {
+                
+        }
+
+        [Fact]
+        public void Add()
+        {
+            Assert.Equal((2 + 2), 4);
+        }
+    }
+}
